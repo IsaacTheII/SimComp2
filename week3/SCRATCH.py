c63b960265eaf982ebbba4a25543d59c675b7a6d
Simon Padua <simon.padua@uzh.ch>
2020-04-03 20:16:12 +0200

    working density without periodic bounary

---
+++ week3/SCRATCH.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Name:      Simon Padua
+   Email:     simon.padua@uzh.ch
+   Date:      01/04/2020
+   Course:    
+   Semester:  
+   Week:      
+   Thema:     
+"""
